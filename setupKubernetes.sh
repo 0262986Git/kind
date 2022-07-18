5b790c1f0caa88d390a53f692369efa742f4843e
0262986Git <78230856+0262986Git@users.noreply.github.com>
2022-07-18 19:39:30 -0400

    Update setupKubernetes.sh

--- setupKubernetes.sh
+++ setupKubernetes.sh
@@ -319,5 +319,4 @@ EOF
 
 ##### Deploy the Agent and Server for a container - B2B Test
 
-kubectl create -f cyperf-agent-client.yaml
-kubectl create -f cyperf-agent-server-nowaf.yaml
+kubectl create -f $HOME/cyperf-agent-client.yaml
